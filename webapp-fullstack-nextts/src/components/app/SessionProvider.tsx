9c28c52da8c59efe1be29e44e47308e17013be79
TranDanh1122 <trandanh14042000@gmail.com>
2025-02-26 09:19:40 +0700

    big upgrade: redux query + redux persist

--- webapp-fullstack-nextts/src/components/app/SessionProvider.tsx
+++ webapp-fullstack-nextts/src/components/app/SessionProvider.tsx
@@ -1,12 +1,19 @@
 'use client'
 
+import { persiststore, store } from "@/redux/store";
 import { Session } from "next-auth";
 import { SessionProvider as SessionContext } from "next-auth/react";
-
-export default function SessionProvider({session, children} : {session : Session | null, children : React.ReactNode}) {
+import { Provider } from "react-redux";
+//@ts-expect-error this librabry @type not working at all
+import { PersistGate } from "redux-persist/integration/react";
+export default function AppServiceProvider({ session, children }: { session: Session | null, children: React.ReactNode }) {
     return (
-        <SessionContext session={session} refetchInterval={5 * 60}>
-            {children}
-        </SessionContext>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persiststore}>
+                <SessionContext session={session} refetchInterval={5 * 60}>
+                    {children}
+                </SessionContext>
+            </PersistGate>
+        </Provider>
     )
 }
--- webapp-fullstack-nextts/src/redux/store.ts
+++ webapp-fullstack-nextts/src/redux/store.ts
@@ -1,1 +1,24 @@
-store.ts
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+//@ts-expect-error this librabry @type not working at all
+import storage from 'redux-persist/lib/storage'
+import { boardAPISlice } from "./slicers/boardSlicer";
+//@ts-expect-error this librabry @type not working at all
+import persistReducer from "redux-persist/es/persistReducer";
+//@ts-expect-error this librabry @type not working at all
+import persistStore from "redux-persist/es/persistStore";
+
+const config = {
+    key: "kanban-task-client-data",
+    storage
+}
+const miniReducers = combineReducers({
+    board: boardAPISlice.reducer
+})
+const localReducer = persistReducer(config, miniReducers)
+export const store = configureStore({
+    reducer: localReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+})
+export const persiststore = persistStore(store)
+export type RootState = ReturnType<typeof store.getState>
+export type AppDisPatch = typeof store.dispatch
